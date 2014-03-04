4d03ee6010dc61ffe2bdbfc1dfb126b1155944fe
pamsi2014pkowalik <piotr.kowalik.pak@gmail.com>
2014-03-04 13:43:50 +0100

    Hello World
    Program wypisujący w konsoli napis "Hello World"

---
+++ pkowalik/Program0/Program0.cpp
@@ -0,0 +1,13 @@
+// Program0.cpp : main project file.
+
+#include "stdafx.h"
+#include <iostream>
+using namespace System;
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+	system("Pause");
+    return 0;
+}
